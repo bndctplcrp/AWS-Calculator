f9a47d77b53047a3c297ceabda52e2004dc483bf
BENEDICT POLICARPIO <142641358+bndctplcrp@users.noreply.github.com>
2023-11-15 14:23:51 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,17 +1,13 @@
 def add(x, y):
-    """Addition Function"""
     return x + y
 
 def subtract(x, y):
-    """Subtraction Function"""
     return x - y
 
 def multiply(x, y):
-    """Multiplication Function"""
     return x * y
 
 def divide(x, y):
-    """Dividion Function"""
     try:
         result = x / y
         return result
@@ -26,10 +22,8 @@ print("3.Multiply")
 print("4.Divide")
 
 while True:
-    # Take input from the user
     choice = input("Enter choice(1/2/3/4): ")
-
-    # Check if choice is one of the four options
+    
     if choice in ('1', '2', '3', '4'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
